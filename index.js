ca14718c16a492befe18a55c433e1ffad602cac0
Lutfi Nailufar Nurdin <lutfi.nurdin@sigma.co.id>
2024-10-26 21:33:06 +0700

    save materi

---
+++ index.js
@@ -0,0 +1,10 @@
+const name = 'Lutfi Nailufar Nurdin'
+const nameArr = name.split(' ')
+
+console.log(nameArr)
+
+let [firstName, middleName,  lastName] = nameArr
+
+console.log(firstName)
+console.log(middleName)
+console.log(lastName)
