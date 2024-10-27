c75ef670f3db829d784d6aa5c5ff54d1fa53aa3b
Lutfi Nailufar Nurdin <lutfi.nurdin@sigma.co.id>
2024-10-27 23:08:47 +0700

    save materi

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,9 +2,39 @@ import './App.css';
 import SearchCard from './SearchCard';
 
 function App() {
+  const listUsers = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "username": "Bret",
+      "email": "Sincere@april.biz"
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "email": "Shanna@melissa.tv"
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "email": "Nathan@yesenia.net",
+    }
+  ]
+
+  const getDataFromChild = (inputValue) => {
+    console.log('dapat data dari child', inputValue)
+  }
+
+  const props = {
+    dataUsers: listUsers,
+    getDataFromChild: getDataFromChild
+  }
+
   return (
     <div className="App">
-      <SearchCard/>
+      <SearchCard {...props} />
     </div>
   );
 }
--- react-app/src/SearchCard.js
+++ react-app/src/SearchCard.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
-const SearchCard = () => {
+const SearchCard = (props) => {
+    console.log(props)
+    const { data, getDataFromChild } = props
     const [inputValue, setInputValue] = useState('')
 
     const handleInputChange = (event) => {
@@ -9,11 +11,19 @@ const SearchCard = () => {
         setInputValue(value)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        getDataFromChild(inputValue)
+    }
+
     return (
-        <form className='SearchCard'>
-            <input type='text' onChange={handleInputChange} value={inputValue} placeholder='Search'></input>
-            <button type='submit'>Search</button>
-        </form>
+        <div>
+            <form className='SearchCard' onSubmit={handleSubmit}>
+                <input type='text' onChange={handleInputChange} value={inputValue} placeholder='Search'></input>
+                <button type='submit'>Search</button>
+            </form>
+
+        </div>
 
     )
 }
