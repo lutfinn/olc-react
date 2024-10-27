89d3a117838e92e3d7cd751220689cd0f62c583a
Lutfi Nailufar Nurdin <lutfi.nurdin@sigma.co.id>
2024-10-27 18:43:14 +0700

    save materi

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
+import SearchCard from './SearchCard';
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       Hello World!
+=======
+      <SearchCard/>
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ react-app/src/SearchCard.js
@@ -0,0 +1,11 @@
+const SearchCard = () => {
+    return (
+        <form className='SearchCard'>
+            <input type='text' placeholder='Search'></input>
+            <button type='submit'>Search</button>
+        </form>
+
+    )
+}
+
+export default SearchCard;
