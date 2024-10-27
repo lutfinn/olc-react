7cbdd168daa5c81e2138b1d81fa2a36e0c00cd9c
Lutfi Nailufar Nurdin <lutfi.nurdin@sigma.co.id>
2024-10-27 23:17:21 +0700

    save materi

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const handleTambah = () => setCount(count + 1)
+  const handleKurang = () => setCount(count - 1)
+  
   return (
     <div className="App">
-      Hello World!
+      <button onClick={handleTambah}>Tambah</button>
+      <button onClick={handleKurang}>Kurang</button>
+      
+      <h1>{count}</h1>
     </div>
   );
 }
