165a240e4f4f349accc0b0e22e0d2940a7d040a7
Lutfi Nailufar Nurdin <lutfi.nurdin@sigma.co.id>
2024-10-27 12:56:07 +0700

    save materi

---
+++ index.js
@@ -0,0 +1,6 @@
+// Menyimpan data
+localStorage.setItem('username', 'react_user');
+
+// Mengambil data
+const username = localStorage.getItem('username');
+console.log(username); // Output: react_user
