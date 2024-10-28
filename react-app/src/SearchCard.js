2f150810313c5029ab488119e274b69e2d812186
Lutfi Nailufar Nurdin <lutfi.nurdin@sigma.co.id>
2024-10-28 10:44:38 +0700

    save materi

--- react-app/src/SearchCard.js
+++ react-app/src/SearchCard.js
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const UsersItem = (props) => {
+    console.log(props)
+    const {user} = props
+    return (
+        <div>
+            <h1>{user.name}</h1>
+            <p>{user.username}</p>
+        </div>
+    )
+}
 
 const SearchCard = (props) => {
-    console.log(props)
-    const { data, getDataFromChild } = props
+    console.log('trigger')
     const [inputValue, setInputValue] = useState('')
+    const [users, setUsers] = useState([])
+
+    const getUserData = async () => {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users?username=${inputValue}`)
+        const result = await response.json()
+        setUsers(result)
+    }
+
+    useEffect(() => {
+        getUserData()
+    }, [inputValue])
 
     const handleInputChange = (event) => {
         const { value } = event.target
@@ -13,8 +34,9 @@ const SearchCard = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        getDataFromChild(inputValue)
     }
+
+    console.log('users', users)
 
     return (
         <div>
@@ -22,7 +44,10 @@ const SearchCard = (props) => {
                 <input type='text' onChange={handleInputChange} value={inputValue} placeholder='Search'></input>
                 <button type='submit'>Search</button>
             </form>
-
+            {users.map((user, index) => (
+                <UsersItem user={user} key={index}/>
+            )
+            )}
         </div>
 
     )
