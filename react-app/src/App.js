c51a66773884603dc4b90f036dc65630223d3722
Lutfi Nailufar Nurdin <lutfi.nurdin@sigma.co.id>
2024-10-29 12:05:24 +0700

    save materi

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
+function Counter() {
   const [count, setCount] = useState(0)
+  const [color, setColor] = useState('salmon')
   const handleTambah = () => setCount(count + 1)
   const handleKurang = () => setCount(count - 1)
+  const handleWarna = () => {
+    const nextColor = color === 'salmon' ? 'blue' : 'salmon'
+    setColor(nextColor)
+  }
+
+  useEffect(() => {
+    console.log(`Sedang didalam useEffect. Count = ${count}`)
+
+    return () => {
+      console.log(`Tugasku membersihkan semuanya`)
+    }
+  }, [count])
 
   return (
     <div className="App">
       <button onClick={handleTambah}>Tambah</button>
+      <button onClick={handleWarna}>Warna</button>
       <button onClick={handleKurang}>Kurang</button>
-      
-      <h1>{count}</h1>
+      <h1 style={{ color }}>{count}</h1>
     </div>
   );
+}
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
 }
 
 export default App;
